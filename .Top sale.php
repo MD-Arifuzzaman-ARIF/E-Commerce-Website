1a6e0ff848f18dd4d1a51191bf60d707006dad6e
Md Arifuzzaman Arif <arifuzzaman.arif@northsouth.edu>
2023-06-05 07:43:41 +0600

    Add files via upload

---
+++ .Top sale.php
@@ -0,0 +1,149 @@
+<!-- Top Sale -->
+<section id="top-sale">
+    <div class="container py-5">
+        <h4 class="font-rubik font-size-20">Top Sale</h4>
+        <hr>
+        <!-- owl carousel -->
+        <div class="owl-carousel owl-theme">
+            <div class="item py-2">
+                <div class="product font-rale">
+                    <a href="#"><img src="./resources/products/1.png" alt="product1" class="img-fluid"></a>
+                    <div class="text-center">
+                        <h6>Energy Bulb</h6>
+                        <div class="rating text-warning font-size-12">
+                            <span><i class="fas fa-star"></i></span>
+                            <span><i class="fas fa-star"></i></span>
+                            <span><i class="fas fa-star"></i></span>
+                            <span><i class="fas fa-star"></i></span>
+                            <span><i class="far fa-star"></i></span>
+                        </div>
+                        <div class="price py-2">
+                            <span>Tk.100</span>
+                        </div>
+                        <button type="submit" class="btn btn-warning font-size-12">Add to Cart</button>
+                    </div>
+                </div>
+            </div>
+            <div class="item py-2">
+                <div class="product font-rale">
+                    <a href="#"><img src="./resources/products/2.png" alt="product1" class="img-fluid"></a>
+                    <div class="text-center">
+                        <h6>TV</h6>
+                        <div class="rating text-warning font-size-12">
+                            <span><i class="fas fa-star"></i></span>
+                            <span><i class="fas fa-star"></i></span>
+                            <span><i class="fas fa-star"></i></span>
+                            <span><i class="fas fa-star"></i></span>
+                            <span><i class="far fa-star"></i></span>
+                        </div>
+                        <div class="price py-2">
+                            <span>Tk.10000</span>
+                        </div>
+                        <button type="submit" class="btn btn-warning font-size-12">Add to Cart</button>
+                    </div>
+                </div>
+            </div>
+            <div class="item py-2">
+                <div class="product font-rale">
+                    <a href="#"><img style="height: 140px;"  src="./resources/products/3.png" alt="product1" class="img-fluid"></a>
+                    <div class="text-center">
+                        <h6>Tester</h6>
+                        <div class="rating text-warning font-size-12">
+                            <span><i class="fas fa-star"></i></span>
+                            <span><i class="fas fa-star"></i></span>
+                            <span><i class="fas fa-star"></i></span>
+                            <span><i class="fas fa-star"></i></span>
+                            <span><i class="far fa-star"></i></span>
+                        </div>
+                        <div class="price py-2">
+                            <span>Tk.50</span>
+                        </div>
+                        <button type="submit" class="btn btn-warning font-size-12">Add to Cart</button>
+                    </div>
+                </div>
+            </div>
+            <div class="item py-2">
+                <div class="product font-rale">
+                    <a href="#"><img style="height: 140px;" src="./resources/products/6.png" alt="product1" class="img-fluid"></a>
+                    <div class="text-center">
+                        <h6>Ac</h6>
+                        <div class="rating text-warning font-size-12">
+                            <span><i class="fas fa-star"></i></span>
+                            <span><i class="fas fa-star"></i></span>
+                            <span><i class="fas fa-star"></i></span>
+                            <span><i class="fas fa-star"></i></span>
+                            <span><i class="far fa-star"></i></span>
+                        </div>
+                        <div class="price py-2">
+                            <span>Tk.45000</span>
+                        </div>
+                        <button type="submit" class="btn btn-warning font-size-12">Add to Cart</button>
+                    </div>
+                </div>
+            </div>
+            <div class="item py-2">
+                <div class="product font-rale">
+                    <a href="#"><img style="height: 140px;" src="./resources/products/4.png" alt="product1" class="img-fluid"></a>
+                    <div class="text-center">
+                        <h6>Refrigerator</h6>
+                        <div class="rating text-warning font-size-12">
+                            <span><i class="fas fa-star"></i></span>
+                            <span><i class="fas fa-star"></i></span>
+                            <span><i class="fas fa-star"></i></span>
+                            <span><i class="fas fa-star"></i></span>
+                            <span><i class="far fa-star"></i></span>
+                        </div>
+                        <div class="price py-2">
+                            <span>Tk.50000</span>
+                        </div>
+                        <button type="submit" class="btn btn-warning font-size-12">Add to Cart</button>
+                    </div>
+                </div>
+            </div>
+            <div class="item py-2">
+                <div class="product font-rale">
+                    <div class="d-flex flex-column">
+                        <a href="#"><img style="height: 140px;" src="./resources/products/5.png" class="img-fluid" alt="pro1"></a>
+                        <div class="text-center">
+                            <h6>Water Filter</h6>
+                            <div class="rating text-warning font-size-12">
+                                <span><i class="fas fa-star"></i></span>
+                                <span><i class="fas fa-star"></i></span>
+                                <span><i class="fas fa-star"></i></span>
+                                <span><i class="fas fa-star"></i></span>
+                                <span><i class="far fa-star"></i></span>
+                            </div>
+                            <div class="price py-2">
+                                <span>Tk.3000</span>
+                            </div>
+                            <button type="submit" class="btn btn-warning font-size-12">Add to Cart</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="item py-2">
+                <div class="product font-rale">
+                    <div class="d-flex flex-column">
+                        <a href="#"><img style="height: 140px;" src="./resources/products/7.png" class="img-fluid" alt="pro1"></a>
+                        <div class="text-center">
+                            <h6>Fan</h6>
+                            <div class="rating text-warning font-size-12">
+                                <span><i class="fas fa-star"></i></span>
+                                <span><i class="fas fa-star"></i></span>
+                                <span><i class="fas fa-star"></i></span>
+                                <span><i class="fas fa-star"></i></span>
+                                <span><i class="far fa-star"></i></span>
+                            </div>
+                            <div class="price py-2">
+                                <span>Tk.1500</span>
+                            </div>
+                            <button type="submit" class="btn btn-warning font-size-12">Add to Cart</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- !owl carousel -->
+    </div>
+</section>
+<!-- !Top Sale -->
