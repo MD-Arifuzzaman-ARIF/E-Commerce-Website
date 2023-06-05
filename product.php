47f79c0a72d7a1d8d471222c4f632fcb7b1a1819
Md Arifuzzaman Arif <arifuzzaman.arif@northsouth.edu>
2023-06-05 07:42:17 +0600

    Add files via upload

---
+++ product.php
@@ -0,0 +1,19 @@
+<?php
+
+// Use to fetch product data
+class Product
+{
+    public $db = null;
+
+    public function __construct(DBController $db)
+    {
+        if (!isset($db->con)) return null;
+        $this->db = $db;
+    }
+
+    // fetch product data using getData Method
+    public function getData($table = 'product')
+    {
+        $result = $this->db->con->query("SELECT * FROM {$table}");
+    }
+}
