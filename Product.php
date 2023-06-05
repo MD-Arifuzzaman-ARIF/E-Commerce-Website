77664ae48b114deebc1efe8cdb9baf97ce238929
Md Arifuzzaman Arif <arifuzzaman.arif@northsouth.edu>
2023-06-05 07:44:24 +0600

    Add files via upload

---
+++ Product.php
@@ -0,0 +1,24 @@
+<?php
+ob_start();
+// include header.php file
+include ('header.php');
+?>
+
+<?php
+
+/*  include banner ads  */
+
+/*  include products  */
+include ('Template/.products.php');
+/*  include products  */
+
+include ('Template/.Top sale.php');
+/*  include banner ads  */
+
+?>
+
+<?php
+// include footer.php file
+include ('footer.php');
+?>
+
---
+++ cart.php
@@ -0,0 +1,23 @@
+<?php
+ob_start();
+// include header.php file
+include ('header.php');
+?>
+
+<?php
+
+/*  include top sale section */
+include ('Template/.cart.php');
+/*  include top sale section */
+
+
+/*  include top sale section */
+include ('Template/.New Phones.php');
+/*  include top sale section */
+
+?>
+
+<?php
+// include footer.php file
+include ('footer.php');
+?>
